25ab443075d259f15d00a703b5bfa4290b541d5c
Ethan Miller <ethanm4984@gmail.com>
2021-01-12 12:45:36 -0700

    dynamically find a vesa video mode

--- src/vesa.asm
+++ src/vesa.asm
@@ -1,22 +1,50 @@
-find_vesa_mode: 
+find_vesa_mode:
     mov ax, 0x4f00
     mov di, VBE_INFO
     int 0x10
     jc .error
 
-    cmp ax, 0x4f
+    cmp ax, 0x4f ; check if we support vbe at all
     jne .error
 
+    cmp dword [VBE_INFO.signature], 'VESA' ; check for vbe version 2+
+    jne .error
+
+    mov ax, word [VBE_INFO.video_mode_seg]
+    mov es, ax
+    mov di, word [VBE_INFO.video_mode_offset]
+.loop:
+    cmp word [es:di], 0xffff
+    je .error
+
+    push es ; some bioses destroy es
+    push di
+
     mov ax, 0x4f01
-    mov cx, 280
+    mov cx, word [es:di]
     mov di, VBE_MODE_INFO
     int 0x10
+
+    pop di
+    pop es
+
+    cmp word [VBE_MODE_INFO.width], 1024
+    jne .back
+
+    cmp word [VBE_MODE_INFO.height], 768
+    jne .back
+
+    cmp byte [VBE_MODE_INFO.bpp], 32
+    jne .back
 
     mov ax, 0x4f02
     mov bx, 280
     int 0x10
 
     ret
+.back:
+    add di, 2
+    jmp .loop
 .error:
     mov si, vesa_error
     call tm_print
